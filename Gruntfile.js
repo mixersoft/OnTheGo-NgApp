965541ab94bdf4ceab64c30f33df815e0a708168
mixersoft <michael@snaphappi.com>
2014-06-17 10:41:58 -0400

    add compass/sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -393,18 +393,18 @@ module.exports = function(grunt) {
     concurrent: {
       server: [
         'coffee:dist',
-        // 'copy:styles'
+        'copy:styles',
         'compass:server'
       ],
       test: [
         'coffee',
-        // 'copy:styles'
+        'copy:styles',
         'compass'
       ],
       dist: [
         'coffee',
-        // 'copy:styles',
-        'compass:dist'
+        'copy:styles',
+        'compass:dist',
         'imagemin',
         'svgmin'
       ]
