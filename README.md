0b9332f8d322949554f95423062e4721c42a3dcf
mixersoft <michael@snaphappi.com>
2014-06-17 09:49:49 -0400

    `yo angular-bootstrap` skeleton with coffee, bootstrap, font-awesome, angular-strap, etc.

---
+++ README.md
@@ -0,0 +1,12 @@
+# OnTheGo
+
+Yet another amazing AngularJS app
+
+## Getting Started
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('null.OnTheGo', ['ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch', 'mgcrea.ngStrap'])
+
+  .constant('version', 'v0.1.0')
+
+  .config(function($locationProvider, $routeProvider) {
+
+    $locationProvider.html5Mode(false);
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html'
+      })
+      .when('/features', {
+        templateUrl: 'views/features.html'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+  });
+
